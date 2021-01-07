e70cd8da28e1581a80b08ba5f2d42e200be33d61
Mykola Toropov <toropovm@gmail.com>
2021-01-07 16:35:24 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,51 @@
+//Q1
+// let selectIdNoQs = document.getElementById("container");
+// console.log(selectIdNoQs);
+
+//Q2
+// let selectedIdQs = document.querySelector("#container");
+// console.log(selectedIdQs);
+
+//Q3
+//  let selectAllId = document.querySelectorAll("li.second");
+//  console.log(selectAllId);
+
+//Q4
+// let selectListItem = document.querySelector("#container ol .third")
+// console.log(selectListItem);
+
+//Q5
+// let assignText = document.getElementById("container");
+// assignText.innerText = "Hello";
+
+//Q6
+// let addClass = document.querySelector("div.footer");
+// addClass.classList.add("main");
+// console.log(addClass);
+
+//Q7
+// let removeClass = document.querySelector("div.footer");
+// removeClass.classList.remove("main");
+// console.log(removeClass);
+
+
+
+//Q8 (must run next 3 together)
+// let createLi = document.createElement("li");
+
+//Q9
+// createLi = "four";
+
+//Q10
+// document.querySelector("ul").append(createLi);
+
+//12
+// let makeGreen = document.querySelectorAll("ol li")
+
+// makeGreen.forEach((element) => { 
+//     element.style.background = "green";
+// })
+
+//13
+let removeDiv = document.querySelector(".footer");
+removeDiv.remove();
