dab047424742095c9446e8d2ee817695c5f9e51b
Mykola Toropov <toropovm@gmail.com>
2021-01-07 16:39:45 -0500

    Final Commit

--- index.js
+++ index.js
@@ -1,50 +1,51 @@
+//Assignment 3 (comment out unused sections)
 //Q1
-// let selectIdNoQs = document.getElementById("container");
-// console.log(selectIdNoQs);
+let selectIdNoQs = document.getElementById("container");
+console.log(selectIdNoQs);
 
 //Q2
-// let selectedIdQs = document.querySelector("#container");
-// console.log(selectedIdQs);
+let selectedIdQs = document.querySelector("#container");
+console.log(selectedIdQs);
 
 //Q3
-//  let selectAllId = document.querySelectorAll("li.second");
-//  console.log(selectAllId);
+ let selectAllId = document.querySelectorAll("li.second");
+ console.log(selectAllId);
 
 //Q4
-// let selectListItem = document.querySelector("#container ol .third")
-// console.log(selectListItem);
+let selectListItem = document.querySelector("#container ol .third")
+console.log(selectListItem);
 
 //Q5
-// let assignText = document.getElementById("container");
-// assignText.innerText = "Hello";
+let assignText = document.getElementById("container");
+assignText.innerText = "Hello";
 
 //Q6
-// let addClass = document.querySelector("div.footer");
-// addClass.classList.add("main");
-// console.log(addClass);
+let addClass = document.querySelector("div.footer");
+addClass.classList.add("main");
+console.log(addClass);
 
 //Q7
-// let removeClass = document.querySelector("div.footer");
-// removeClass.classList.remove("main");
-// console.log(removeClass);
+let removeClass = document.querySelector("div.footer");
+removeClass.classList.remove("main");
+console.log(removeClass);
 
 
 
 //Q8 (must run next 3 together)
-// let createLi = document.createElement("li");
+let createLi = document.createElement("li");
 
 //Q9
-// createLi = "four";
+createLi = "four";
 
 //Q10
-// document.querySelector("ul").append(createLi);
+document.querySelector("ul").append(createLi);
 
 //12
-// let makeGreen = document.querySelectorAll("ol li")
+let makeGreen = document.querySelectorAll("ol li")
 
-// makeGreen.forEach((element) => { 
-//     element.style.background = "green";
-// })
+makeGreen.forEach((element) => { 
+    element.style.background = "green";
+})
 
 //13
 let removeDiv = document.querySelector(".footer");
